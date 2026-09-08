d7802dcfaff922c0f80fc8005e5a746f3bf36849
Puppy-4 <259552040+The-Puppy-Artist@users.noreply.github.com>
2026-09-08 16:16:44 +0800

    New practice done, time to review this for haha.

---
+++ Practice4_SchoolTask2/src/Practice4_SchoolTask2.java
@@ -0,0 +1,40 @@
+public class Practice4_SchoolTask2{
+    public static double toInch (double cm){
+        return 0.394*(cm);
+    }
+
+    public static double computeAreaOfCircle(double r){
+        float pi;
+        pi = 3.1415f;
+        return 3.1415*(r*r);
+    }
+
+    public static boolean isPass(double grade, double pass){
+        if (grade < 70) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int sumToTarget(int target){
+        return (target*(target+1))/2;
+    }
+    public static int lastDigit(int num){
+        return num%10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("A: "+ toInch(2.5f));
+        System.out.println("B: "+ toInch(10f));
+        System.out.println("A: "+ computeAreaOfCircle(2.5f));
+        System.out.println("B: "+ computeAreaOfCircle(10));
+        System.out.println("A: "+ isPass(80, 70));
+        System.out.println("B: "+ isPass(60, 70));
+        System.out.println("A: "+ sumToTarget(9));
+        System.out.println("B: "+ sumToTarget(16));
+        System.out.println("A: "+ lastDigit(9));
+        System.out.println("B: "+ lastDigit(16));
+    }
+
+}
